302e68d85a301c12ba7742b0d574e0864240951b
Josh Suereth <joshua.suereth@gmail.com>
2019-11-12 12:04:27 -0500

    Attempt to clean up type signatures and fragment out the logic.
    - We want methods that are 'boilerplate' and 'fill in the details'
    - We want to clearly denote ZIO[Env,Err,Result] types in all signatures for teaching.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,34 @@
 import java.util.UUID
 
-import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.{
+  ProducerRecord, 
+  RecordMetadata
+}
 import sttp.client._
 import sttp.client.asynchttpclient.zio.ZioWebSocketHandler
 import sttp.client.asynchttpclient.ziostreams.AsyncHttpClientZioStreamsBackend
 import sttp.model.Uri
 import zio._
+import zio.blocking.Blocking
 import zio.kafka.client.{Producer, ProducerSettings}
 import zio.duration._
 import zio.kafka.client.serde.Serde
+import sttp.client.ws.{
+  WebSocketResponse,
+  WebSocketEvent
+}
+import sttp.model.ws.WebSocketFrame
+
+
+/** Configuration for our web-service. */
+case class Config(bootstrapServer: String, kafkaTopic: String, wsServer: Uri)
 
 object Main extends App {
+  type MyRecord = ProducerRecord[String, String]
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
     wsToKafka.fold(_ => 1, _ => 0)
   }
-
-  case class Config(bootstrapServer: String, kafkaTopic: String, wsServer: Uri)
 
   val config = for {
     bootstrapServer <- system.env("BOOTSTRAP_SERVER").someOrFail()
@@ -24,8 +36,9 @@ object Main extends App {
     wsServer <- system.env("WS_SERVER").someOrFail().flatMap { wsServer => ZIO.fromTry(Uri.parse(wsServer)) }
   } yield Config(bootstrapServer, kafkaTopic, wsServer)
 
+
   // todo: exit doesn't work
-  val wsToKafka = config.flatMap { config =>
+  val wsToKafka: ZIO[ZEnv, Any, Nothing] = config.flatMap { config =>
     val producerSettings = ProducerSettings(
       bootstrapServers= List(config.bootstrapServer),
       closeTimeout = 30.seconds,
@@ -33,26 +46,33 @@ object Main extends App {
     )
 
     // todo: make[Any, _, _] is needed otherwise we get ZIO[Nothing with Blocking, _, _] and that doesn't work.
-    Producer.make[Any, String, String](producerSettings, Serde.string, Serde.string).use { producer =>
-      ZIO.runtime[ZEnv].flatMap { runtime =>
-        val sttpBackendTask = AsyncHttpClientZioStreamsBackend.usingConfigBuilder(runtime, _.setWebSocketMaxFrameSize(1024 * 1024))
-
-        sttpBackendTask.flatMap { implicit sttpBackend =>
-          ZioWebSocketHandler().flatMap { webSocketHandler =>
-            basicRequest.get(config.wsServer).openWebsocket(webSocketHandler).flatMap { response =>
-              response.result.receiveText().flatMap {
-                case Left(_) =>
-                  ZIO.interrupt // todo: is this the right way to close?
-                case Right(text) =>
-                  // todo: not UUID for key - question URL?
-                  val producerRecord = new ProducerRecord(config.kafkaTopic, UUID.randomUUID().toString, text.payload)
-                  producer.produce(producerRecord)
-              }.forever
-            }
-          }
-        }
-      }
-    }
+    Producer.make[Any, String, String](producerSettings, Serde.string, Serde.string).use(handleConnections(config))
   }
 
+  // This method takes in config + a kafka Producer, and will handle all incoming websocket connection, forever.
+  // We return "Nothing" because we never return, infinitely handling connections.
+  def handleConnections(config: Config)(producer: Producer[Any, String, String]): ZIO[ZEnv, Throwable, Nothing] =
+    for {
+        runtime <- ZIO.runtime[ZEnv]
+        sttpBackendTask = AsyncHttpClientZioStreamsBackend.usingConfigBuilder(runtime, _.setWebSocketMaxFrameSize(1024 * 1024))
+        sttpBackend <- sttpBackendTask
+        webSocketHandler <- ZioWebSocketHandler()
+        response <- basicRequest.get(config.wsServer).openWebsocket(webSocketHandler)(sttpBackend, implicitly)
+        text <- response.result.receiveText()
+        result <- handleConnection(config.kafkaTopic, producer)(text).forever
+      } yield result
+
+
+  // Handles a websocket connection
+  // TODO - look into hiding the Producer in the ZIO environment.
+  def handleConnection(kafkaTopic: String, producer: Producer[Any, String, String])(
+    text: Either[WebSocketEvent.Close,WebSocketFrame.Text]): ZIO[Any with Blocking, Throwable, Task[RecordMetadata]] =
+      text match {
+        case Left(_) =>
+          ZIO.interrupt // todo: is this the right way to close?
+        case Right(text) =>
+          // todo: not UUID for key - question URL?
+          val producerRecord = new ProducerRecord(kafkaTopic, UUID.randomUUID().toString, text.payload)
+          producer.produce(producerRecord)
+      }
 }
